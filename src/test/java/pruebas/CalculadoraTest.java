4470483076d2bcd1cae9141033ccb7fb6829034a
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-04-30 13:21:11 -0500

    agregar ejemplo

---
+++ src/test/java/pruebas/CalculadoraTest.java
@@ -0,0 +1,20 @@
+package pruebas;
+
+import clases.Calculadora;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSuma() {
+        Calculadora calculadora = new Calculadora();
+        double a = 4;
+        double b = 2;
+        double total = a + b;
+        double suma = calculadora.suma(a,b);
+        assertEquals(suma,total);
+    }
+
+}
